0ae8da39937da17e24762083f4dbcaec9db10ab0
avven1re <keithfez@gmail.com>
2019-11-27 14:21:47 +0800

    modified

--- code/how_to_get_API.R
+++ code/how_to_get_API.R
@@ -9,11 +9,11 @@ browseURL("https://steamdb.info/") # search app id in Steam
 
 
 #examples
-csgo_api <- GET("steamspy.com/api.php?request=appdetails&appid=730") # CSGO's app id : 730
+##csgo_api <- GET("steamspy.com/api.php?request=appdetails&appid=730") # CSGO's app id : 730
 
-csgo <- content(csgo_api, as = "parsed") #?content for details. use <as = "parsed"> to make a list for R
+##csgo <- content(csgo_api, as = "parsed") #?content for details. use <as = "parsed"> to make a list for R
 
-csgo #done
+##csgo #done
 
 
 #function
@@ -23,7 +23,14 @@ gameAPI <- function(id, As = "parsed"){
   URLpatse <- paste0("steamspy.com/api.php?request=appdetails&appid=", id)
   game_api_source <- GET(URLpatse)
   game_api <- content(game_api_source, as = As)
+  
+  if(sum(is.null(game_api$name)) == 1){
+    stop("Browse URL:", 
+    select <- askYesNo("Do you want to open Steamdb? (https://steamdb.info/)"), 
+    if(select == 1) browseURL("https://steamdb.info/"), "\n",  "Error : App ID is missing, please import correctly or find it at https://steamdb.info/")
+    }
+  
   game_api
 }
 
-cs <- gameAPI(730)
+#cs <- gameAPI(730)
