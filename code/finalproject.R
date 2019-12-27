437974e2a74796621c76ec7535e626441ab05c81
avven1re <keithfez@gmail.com>
2019-12-27 17:09:54 +0800

    modified

--- code/finalproject.R
+++ code/finalproject.R
@@ -63,7 +63,7 @@ ani_OECD <- t_meanOECD %>%
   ggplot(aes(x = agegp, y = value, color = variable)) +
   geom_line(size = 1.2) + 
   geom_point(size = 2) +
-  scale_color_discrete(name = "問卷題目(1到10分)", labels = rev(c("請問您昨天覺得快樂嗎？", "請問您昨天覺得擔憂嗎？", "請問您昨天覺得沮喪嗎？", 
+  scale_color_discrete(name = "問卷題目(滿意度低到高1到10分)", labels = rev(c("請問您昨天覺得快樂嗎？", "請問您昨天覺得擔憂嗎？", "請問您昨天覺得沮喪嗎？", 
                                                     "您對於目前生活的滿意程度", "對於人生當中所有做過的事情值得嗎?", "您對自己生活水準的滿意程度", 
                                                     "對自己健康狀況", "對自己人生的成就", "對自己的人際關係", "請問您對自己安全感受的滿意程度", 
                                                     "您對自己歸屬於社區一份子的滿意程度", "對自己未來生活的保障", 
@@ -73,6 +73,7 @@ ani_OECD <- t_meanOECD %>%
   scale_x_discrete(limits = 1 : 6, labels= c( "15-24 歲", "25-34 歲", "35-44 歲", "45-54 歲", "55-64 歲", "65 歲以上")) +
   theme(axis.text.x = element_text(angle = 45), axis.text=element_text(size = 18)) +
   ylim(c(0, 10)) +
+  ggtitle("社會聯繫與生活層面滿意度") +
   transition_reveal(agegp) 
 
   animate(ani_OECD, height = 500, width = 650)
@@ -91,13 +92,14 @@ ani_OECD <- t_meanOECD %>%
     ggplot(aes(x = agegp, y = value, color = variable)) +
     geom_line(size = 1.2) + 
     geom_point(size = 2) +
-    scale_color_discrete(name = "問卷題目(高到低:1到4分)", labels = rev(c("請問您對立法院信任或不信任？", "對司法制度及法院?", "對其他的中央政府?", 
+    scale_color_discrete(name = "問卷題目(信任度高到低:1到4分)", labels = rev(c("請問您對立法院信任或不信任？", "對司法制度及法院?", "對其他的中央政府?", 
                                                              "對您居住地區的地方政府?", "對媒體的品質及公正性?", "請問您對我國的社會保障制度信不信任？"))) +
     xlab("年齡分布") +
     ylab("平均值") +
     scale_x_discrete(limits = 1 : 6, labels= c( "15-24 歲", "25-34 歲", "35-44 歲", "45-54 歲", "55-64 歲", "65 歲以上")) +
     theme(axis.text.x = element_text(angle = 45), axis.text=element_text(size = 18)) +
     ylim(c(4, 0)) +
+    ggtitle("政府機構信任程度") + 
     transition_reveal(agegp) 
   
   animate(ani_OECD2, height = 500, width = 650)
@@ -113,38 +115,39 @@ ani_OECD <- t_meanOECD %>%
     ggplot(aes(x = agegp, y = value, color = variable)) +
     geom_line(size = 1.2) + 
     geom_point(size = 2) +
-    scale_color_discrete(name = "問卷題目(高到低:1到4分)", labels = rev(c("請問您滿不滿意您在我國所擁有的民主生活？", "請問您滿不滿意我國的言論自由?", 
+    scale_color_discrete(name = "問卷題目(信任度高到低:1到4分)", labels = rev(c("請問您滿不滿意您在我國所擁有的民主生活？", "請問您滿不滿意我國的言論自由?", 
                                                                  "政府官員會重視我們一般老百姓的想法，是否同意?"))) +
     xlab("年齡分布") +
     ylab("平均值") +
     scale_x_discrete(limits = 1 : 6, labels= c( "15-24 歲", "25-34 歲", "35-44 歲", "45-54 歲", "55-64 歲", "65 歲以上")) +
     theme(axis.text.x = element_text(angle = 45), axis.text=element_text(size = 18)) +
     ylim(c(4, 0)) +
+    ggtitle("國家情勢信任程度") + 
     transition_reveal(agegp) 
   
   animate(ani_OECD3, height = 500, width = 650)
   anim_save("images/age_to_v37-39.gif")
   
 #animated plot (by edu)
-  #animated plot v7-v20
-  meanOECD <- aggregate(v7 ~ agegp, nOECD, mean, na.rm = T)
-  meanOECD <- merge(aggregate(v8 ~ agegp, nOECD, mean, na.rm = T), meanOECD, by = "agegp")
-  meanOECD <- merge(aggregate(v9 ~ agegp, nOECD, mean, na.rm = T), meanOECD, by = "agegp")
-  meanOECD <- merge(aggregate(v10 ~ agegp, nOECD, mean, na.rm = T), meanOECD, by = "agegp")
-  meanOECD <- merge(aggregate(v11 ~ agegp, nOECD, mean, na.rm = T), meanOECD, by = "agegp")
-  meanOECD <- merge(aggregate(v12 ~ agegp, nOECD, mean, na.rm = T), meanOECD, by = "agegp")
-  meanOECD <- merge(aggregate(v13 ~ agegp, nOECD, mean, na.rm = T), meanOECD, by = "agegp")
-  meanOECD <- merge(aggregate(v14 ~ agegp, nOECD, mean, na.rm = T), meanOECD, by = "agegp")
-  meanOECD <- merge(aggregate(v15 ~ agegp, nOECD, mean, na.rm = T), meanOECD, by = "agegp")
-  meanOECD <- merge(aggregate(v16 ~ agegp, nOECD, mean, na.rm = T), meanOECD, by = "agegp")
-  meanOECD <- merge(aggregate(v17 ~ agegp, nOECD, mean, na.rm = T), meanOECD, by = "agegp")
-  meanOECD <- merge(aggregate(v18 ~ agegp, nOECD, mean, na.rm = T), meanOECD, by = "agegp")
-  meanOECD <- merge(aggregate(v19 ~ agegp, nOECD, mean, na.rm = T), meanOECD, by = "agegp")
-  meanOECD <- merge(aggregate(v20 ~ agegp, nOECD, mean, na.rm = T), meanOECD, by = "agegp")
-  t_meanOECD <- reshape2::melt(meanOECD, id = "agegp")
+  #animated plot 4 v7-v20
+  meanOECD4 <- aggregate(v7 ~ edugp, nOECD, mean, na.rm = T)
+  meanOECD4 <- merge(aggregate(v8 ~ edugp, nOECD, mean, na.rm = T), meanOECD4, by = "edugp")
+  meanOECD4 <- merge(aggregate(v9 ~ edugp, nOECD, mean, na.rm = T), meanOECD4, by = "edugp")
+  meanOECD4 <- merge(aggregate(v10 ~ edugp, nOECD, mean, na.rm = T), meanOECD4, by = "edugp")
+  meanOECD4 <- merge(aggregate(v11 ~ edugp, nOECD, mean, na.rm = T), meanOECD4, by = "edugp")
+  meanOECD4 <- merge(aggregate(v12 ~ edugp, nOECD, mean, na.rm = T), meanOECD4, by = "edugp")
+  meanOECD4 <- merge(aggregate(v13 ~ edugp, nOECD, mean, na.rm = T), meanOECD4, by = "edugp")
+  meanOECD4 <- merge(aggregate(v14 ~ edugp, nOECD, mean, na.rm = T), meanOECD4, by = "edugp")
+  meanOECD4 <- merge(aggregate(v15 ~ edugp, nOECD, mean, na.rm = T), meanOECD4, by = "edugp")
+  meanOECD4 <- merge(aggregate(v16 ~ edugp, nOECD, mean, na.rm = T), meanOECD4, by = "edugp")
+  meanOECD4 <- merge(aggregate(v17 ~ edugp, nOECD, mean, na.rm = T), meanOECD4, by = "edugp")
+  meanOECD4 <- merge(aggregate(v18 ~ edugp, nOECD, mean, na.rm = T), meanOECD4, by = "edugp")
+  meanOECD4 <- merge(aggregate(v19 ~ edugp, nOECD, mean, na.rm = T), meanOECD4, by = "edugp")
+  meanOECD4 <- merge(aggregate(v20 ~ edugp, nOECD, mean, na.rm = T), meanOECD4, by = "edugp")
+  t_meanOECD4 <- reshape2::melt(meanOECD4, id = "edugp")
   
-  ani_OECD <- t_meanOECD %>%
-    ggplot(aes(x = agegp, y = value, color = variable)) +
+  ani_OECD4 <- t_meanOECD4 %>%
+    ggplot(aes(x = edugp, y = value, color = variable)) +
     geom_line(size = 1.2) + 
     geom_point(size = 2) +
     scale_color_discrete(name = "問卷題目(1到10分)", labels = rev(c("請問您昨天覺得快樂嗎？", "請問您昨天覺得擔憂嗎？", "請問您昨天覺得沮喪嗎？", 
@@ -152,12 +155,62 @@ ani_OECD <- t_meanOECD %>%
                                                               "對自己健康狀況", "對自己人生的成就", "對自己的人際關係", "請問您對自己安全感受的滿意程度", 
                                                               "您對自己歸屬於社區一份子的滿意程度", "對自己未來生活的保障", 
                                                               "請問您對可以做自己喜歡事情的時間長短", "您對居住地區環境品質的滿意程度"))) +
-    xlab("年齡分布") +
+    xlab("教育程度分布") +
     ylab("平均值") +
-    scale_x_discrete(limits = 1 : 6, labels= c( "15-24 歲", "25-34 歲", "35-44 歲", "45-54 歲", "55-64 歲", "65 歲以上")) +
+    scale_x_discrete(limits = 1 : 3, labels= c( "國中小及以下", "高中(職)含五專前 3 年", "大專以上")) +
     theme(axis.text.x = element_text(angle = 45), axis.text=element_text(size = 18)) +
     ylim(c(0, 10)) +
-    transition_reveal(agegp) 
+    ggtitle("社會聯繫與生活層面滿意度") +
+    transition_reveal(edugp) 
   
-  animate(ani_OECD, height = 500, width = 650)
+  animate(ani_OECD4, height = 500, width = 650)
   anim_save("images/edu_to_v7-20.gif")
+
+  #animated plot 5 v31-v36
+  meanOECD5 <- aggregate(v31 ~ edugp, nOECD, mean, na.rm = T)
+  meanOECD5 <- merge(aggregate(v32 ~ edugp, nOECD, mean, na.rm = T), meanOECD5, by = "edugp")
+  meanOECD5 <- merge(aggregate(v33 ~ edugp, nOECD, mean, na.rm = T), meanOECD5, by = "edugp")
+  meanOECD5 <- merge(aggregate(v34 ~ edugp, nOECD, mean, na.rm = T), meanOECD5, by = "edugp")
+  meanOECD5 <- merge(aggregate(v35 ~ edugp, nOECD, mean, na.rm = T), meanOECD5, by = "edugp")
+  meanOECD5 <- merge(aggregate(v36 ~ edugp, nOECD, mean, na.rm = T), meanOECD5, by = "edugp")
+  t_meanOECD5 <- reshape2::melt(meanOECD5, id = "edugp")
+  
+  ani_OECD5 <- t_meanOECD5 %>%
+    ggplot(aes(x = edugp, y = value, color = variable)) +
+    geom_line(size = 1.2) + 
+    geom_point(size = 2) +
+    scale_color_discrete(name = "問卷題目(信任度高到低:1到4分)", labels = rev(c("請問您對立法院信任或不信任？", "對司法制度及法院?", "對其他的中央政府?", 
+                                                                    "對您居住地區的地方政府?", "對媒體的品質及公正性?", "請問您對我國的社會保障制度信不信任？"))) +
+    xlab("教育程度分布") +
+    ylab("平均值") +
+    scale_x_discrete(limits = 1 : 3, labels= c( "國中小及以下", "高中(職)含五專前 3 年", "大專以上")) +
+    theme(axis.text.x = element_text(angle = 45), axis.text=element_text(size = 18)) +
+    ylim(c(4, 0)) +
+    ggtitle("政府機構信任程度") + 
+    transition_reveal(edugp) 
+  
+  animate(ani_OECD5, height = 500, width = 650)
+  anim_save("images/edu_to_v31-36.gif")
+  
+  #animated plot 6 v37-v39
+  meanOECD6 <- aggregate(v37 ~ edugp, nOECD, mean, na.rm = T)
+  meanOECD6 <- merge(aggregate(v38 ~ edugp, nOECD, mean, na.rm = T), meanOECD6, by = "edugp")
+  meanOECD6 <- merge(aggregate(v39 ~ edugp, nOECD, mean, na.rm = T), meanOECD6, by = "edugp")
+  t_meanOECD6 <- reshape2::melt(meanOECD6, id = "edugp")
+  
+  ani_OECD6 <- t_meanOECD6 %>%
+    ggplot(aes(x = edugp, y = value, color = variable)) +
+    geom_line(size = 1.2) + 
+    geom_point(size = 2) +
+    scale_color_discrete(name = "問卷題目(信任度高到低:1到4分)", labels = rev(c("請問您滿不滿意您在我國所擁有的民主生活？", "請問您滿不滿意我國的言論自由?", 
+                                                                    "政府官員會重視我們一般老百姓的想法，是否同意?"))) +
+    xlab("教育程度分布") +
+    ylab("平均值") +
+    scale_x_discrete(limits = 1 : 3, labels= c( "國中小及以下", "高中(職)含五專前 3 年", "大專以上")) +
+    theme(axis.text.x = element_text(angle = 45), axis.text=element_text(size = 18)) +
+    ylim(c(4, 0)) +
+    ggtitle("國家情勢信任程度") + 
+    transition_reveal(edugp) 
+  
+  animate(ani_OECD6, height = 500, width = 650)
+  anim_save("images/edu_to_v37-39.gif")
