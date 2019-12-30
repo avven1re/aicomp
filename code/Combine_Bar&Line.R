a8b0f8269ee36a6f31b9b6f2a0a7b01a8915495b
avven1re <keithfez@gmail.com>
2019-12-30 19:24:53 +0800

    modified

--- code/Combine_Bar&Line.R
+++ code/Combine_Bar&Line.R
@@ -38,14 +38,15 @@ t_meanOECD <- reshape2::melt(meanOECD, id = "agegp")
 
 ani_OECD <- t_meanOECD[1 : 84, ] %>%
   ggplot(aes(x = agegp, y = value, color = variable)) +
-  geom_bar(t_meanOECD[85 : 90, ], mapping = aes(x = agegp, y = value/sum(value) * 10, fill = agegp), stat = "identity", na.rm = T) +
+  geom_bar(t_meanOECD[85 : 90, ], mapping = aes(x = agegp, y = value/sum(value) * 10, fill = agegp), stat = "identity") +
   geom_line(size = 1.2) + 
   geom_point(size = 2) +
   scale_color_discrete(name = "問卷題目(滿意度低到高1到10分)", labels = rev(c("請問您昨天覺得快樂嗎？", "請問您昨天覺得擔憂嗎？", "請問您昨天覺得沮喪嗎？", 
                                                                   "您對於目前生活的滿意程度", "對於人生當中所有做過的事情值得嗎?", "您對自己生活水準的滿意程度", 
                                                                   "對自己健康狀況", "對自己人生的成就", "對自己的人際關係", "請問您對自己安全感受的滿意程度", 
                                                                   "您對自己歸屬於社區一份子的滿意程度", "對自己未來生活的保障", 
-                                                                  "請問您對可以做自己喜歡事情的時間長短", "您對居住地區環境品質的滿意程度"))) +
+                                                                  "請問您對可以做自己喜歡事情的時間長短", "您對居住地區環境品質的滿意程度", "人數百分比長條圖(位於底部無折線)"))) +
+  scale_fill_continuous(guide=FALSE) +
   xlab("年齡分布") +
   ylab("平均值") +
   scale_x_discrete(limits = 1 : 6, labels= c( "15-24 歲", "25-34 歲", "35-44 歲", "45-54 歲", "55-64 歲", "65 歲以上")) +
@@ -56,7 +57,7 @@ ani_OECD <- t_meanOECD[1 : 84, ] %>%
   transition_reveal(agegp) 
 
 animate(ani_OECD, height = 500, width = 650)
-#anim_save("images/age_to_v7-20.gif")
+anim_save("images/age_to_v7-20_ver2.gif")
 
 #animated plot 2 v31-v36
 meanOECD2 <- aggregate(v31 ~ agegp, nOECD, mean, na.rm = T)
