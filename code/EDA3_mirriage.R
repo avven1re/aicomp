69d747200c351b315659a1905a6f41bd43d601e3
avven1re <keithfez@gmail.com>
2020-01-01 14:05:45 +0800

    add

---
+++ code/EDA3_mirriage.R
@@ -0,0 +1,19 @@
+#encoding : UTF-8
+#packages
+library(devtools)
+#install.packages("readr")
+library(readr)
+library(ggplot2)
+library(gganimate)
+library(magrittr)
+library("reshape2")
+#Import data
+{
+  guess_encoding("dataset/Happiness.csv", n_max = 1000)
+  nOECD <- read.csv("dataset/Happiness.csv")
+  names(nOECD)[1] <- c("v1")
+  names(nOECD)
+  
+  OECD_weight <- as.vector(nOECD$weight)
+  nOECD2 <- as.data.frame(OECD_weight * as.matrix(nOECD[, 2:39]))
+  nOECD <- as.data.frame(cbind(nOECD2, nOECD[, 40:44]))}
