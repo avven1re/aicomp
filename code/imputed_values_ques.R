1791917528fc4c26ff243b3d7135e2bcc956c692
OscarC <oscarchen1998531@gmail.com>
2020-01-03 17:46:34 +0800

    add

---
+++ code/imputed_values_ques.R
@@ -0,0 +1,27 @@
+## °İ¨÷
+ques = read.csv("dataset/Happiness.csv")  
+ques[,1] <- 109-ques[,1]
+names(ques)[1:41] <- c("age",paste('q',1:38,sep=''),"marrgp","incogp")
+names(ques)[46] <- "TOWNCODE"
+head(ques)
+
+# install.packages("missForest")
+library(missForest)
+#seed missing values ( 5% )
+ques.mis <- prodNA(ques[,c(6:19, 30:38)], noNA = 0.05)
+summary(ques.mis)
+
+#impute missing values, using all parameters as default values
+ques.imp <- missForest(ques.mis)
+
+#check imputed values
+ques.imp$ximp
+
+#check imputation error
+ques.imp$OOBerror
+
+#comparing actual data accuracy
+ques.err <- mixError(ques.imp$ximp, ques.mis, ques)
+ques.err
+
+write.csv(ques.imp$ximp,file="dataset/data_missing.csv",row.names = FALSE)
