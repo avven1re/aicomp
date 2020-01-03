3989b94a04f929abca19f5f1165e06ea1a675190
avven1re <keithfez@gmail.com>
2020-01-03 17:22:34 +0800

    add

--- code/d_reduction.R
+++ code/d_reduction.R
@@ -1,1 +1,47 @@
 # encoding : UTF-8
+#Land
+r_ld2_d <- as.data.frame(ld2[, 3 : 66])
+
+r_ld2 <- princomp(r_ld2_d)
+
+biplot(r_ld2)
+
+barplot(r_ld2$scores)
+
+#install.packages("FactoMineR")
+library(FactoMineR)
+#install.packages("factoextra")
+library(factoextra)
+#install.packages(corrplot)
+library(corrplot)
+library(ggplot2)
+
+
+r_ld2 <- PCA(r_ld2_d)
+
+fviz_eig(r_ld2)
+
+#PCA
+#Question1
+r_ques1_d <- as.data.frame(ques[, 7:11])
+r_ques1 <- PCA(r_ques1_d)
+fviz_eig(r_ques1)
+
+#Question2
+r_ques2_d <- as.data.frame(ques[, 12 : 20])
+r_ques2 <- PCA(r_ques2_d)
+fviz_eig(r_ques2)
+
+#Question3
+r_ques3_d <- as.data.frame(ques[, 31 : 39])
+r_ques3 <- PCA(r_ques3_d)
+fviz_eig(r_ques3)
+
+#isomap
+library(vegan)
+i_r_ques1 <- isomapdist(dist(r_ques1_d), ndim = 2, k = 11, fragmentedOK = T)
+
+
+i_r_ques2 <- isomap(dist(r_ques2_d), ndim = 2, k = 5, fragmentedOK = T)
+
+i_r_ques3 <- isomap(dist(r_ques3_d), ndim = 2, k = 5, fragmentedOK = T)
