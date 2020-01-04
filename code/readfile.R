399ce582d84f723dd3d3aa4c4d3c2eef975a8763
HaythamLee <arutearisu@gmail.com>
2020-01-04 15:59:55 +0800

    modded

--- code/readfile.R
+++ code/readfile.R
@@ -10,15 +10,18 @@ head(ques)
 
 ## 土地利用資料
 ld1=read.csv("dataset/landdata.csv", skip=1, header=F, fileEncoding = "UTF-8-BOM")
-ld2 <- ld1[-1]
-names(ld2) <- c("county","town","area",paste('x',1:63,sep=''))
-for (i in 1:dim(ld2)[1]){
-  for (j in 4:dim(ld2)[2]){
-    ld2[i,j] <- ld2[i,j]/ld2[i,3]
+for (i in 1:dim(ld1)[1]){
+  for (j in 4:dim(ld1)[2]){
+    ld1[i,j] <- ld1[i,j]/ld1[i,3]
   }
 }
+# head(ld1)
+ld2 <- cbind(ld1[,1:3], rowSums(ld1[,4:9]), rowSums(ld1[,10:16]), rowSums(ld1[,17:27])
+             , rowSums(ld1[,28:38]), rowSums(ld1[,39:47]), rowSums(ld1[,48:53])
+             , rowSums(ld1[,54:57]), rowSums(ld1[,58:60]), rowSums(ld1[,61:66]))
+names(ld2) <- c("county","town","area","agri","forest","traffic"
+                ,"water","building","gov","leisure","mine","other")
 head(ld2)
-
 ## 鄉鎮分區界線
 taiwan.town.map <- st_read("dataset/town/TOWN_MOI_1070205.shp")
 head(taiwan.town.map)
