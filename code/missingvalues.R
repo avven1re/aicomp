9566beff462b6822b3ed7f2cba45629aec891d7e
OscarC <oscarchen1998531@gmail.com>
2020-01-04 14:37:59 +0800

    modified

--- code/missingvalues.R
+++ code/missingvalues.R
@@ -5,3 +5,9 @@ dataA.aggrplot<-aggr(dataA[,1:112], col=c('lightblue','red'),
                       labels=names(dataA), cex.axis=.7, gap=3)
 # q21 為 missing by design
 # missing at random < 10%, do not need to impute values
+dataB.aggrplot<-aggr(dataB, col=c('lightblue','red'), 
+                     numbers=TRUE, prop = TRUE, sortVars=TRUE, 
+                     labels=names(dataB), cex.axis=.7, gap=3)
+dataC.aggrplot<-aggr(dataC, col=c('lightblue','red'), 
+                     numbers=TRUE, prop = TRUE, sortVars=TRUE, 
+                     labels=names(dataA), cex.axis=.7, gap=3)
