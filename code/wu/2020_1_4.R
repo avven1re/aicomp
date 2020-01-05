edfa77207cfef9d11a5259ab70dac08fc47d5524
scwu110927 <scwu110927@gmail.com>
2020-01-05 17:20:24 +0800

    modify

--- code/wu/2020_1_4.R
+++ code/wu/2020_1_4.R
@@ -63,17 +63,20 @@ a.Nb <- NbClust(a.comp$completeObs, distance = "euclidean",
                 min.nc=2, max.nc=10, method = "kmeans", 
                 index = "dunn")
 a.Nb$All.index #Max
+set.seed(123)
 a.kmeans <- kmeans(a.comp$completeObs, 4, 20)
 
  # Cluster b => 3 cluster
 fviz_nbclust(b.comp$completeObs, kmeans, method = "wss", 
              k.max = 10) + theme_minimal()
+set.seed(123)
 b.kmeans <- kmeans(b.comp$completeObs, 3, 20)
 
  # Cluster c => 4 cluster
 c.Nb <- NbClust(c.comp$completeObs, distance = "euclidean", 
                 min.nc=2, max.nc=10, method = "kmeans", index = "sdbw")
 c.Nb$All.index #Min
+set.seed(123)
 c.kmeans <- kmeans(c.comp$completeObs, 4, 20)
 
  # Cluster Land => 7 cluster
@@ -82,18 +85,15 @@ land.Nb <- NbClust(dataA[, 1:63], distance = "euclidean",
 land.Nb$All.index
 set.seed(123)
 land.kmeans <- kmeans(dataA[, 1:63], 7, 50)
-# plot(land.kmeans$centers[1,], type = "b", ylim=c(0,1000), xlim=c(0,65)) #black 
-# points(land.kmeans$centers[2,], col=2, type = "b") #red 38 40 45 46 63 工業住宅
-# points(land.kmeans$centers[3,], col=3, type = "b") #green
-# points(land.kmeans$centers[4,], col=4, type = "b") #blue 西部平原農業區 1 2 22 32 38 40 60 61 63 
-# points(land.kmeans$centers[5,], col=5, type = "b") #lblue 11 丘陵
-# points(land.kmeans$centers[6,], col=6, type = "b") #purple 9 22 全台山地
-# points(land.kmeans$centers[7,], col=7, type = "b") #gray 24 35 53 南部濱海
-# test <- cbind(dataA, land.kmeans$cluster)
-# test[land.kmeans$cluster==7, 2:3]
-
-
-
+ # plot(land.kmeans$centers[1,], type = "b", ylim=c(0,1000), xlim=c(0,65)) #black 
+ # points(land.kmeans$centers[2,], col=2, type = "b") #red 38 40 45 46 63 工業住宅
+ # points(land.kmeans$centers[3,], col=3, type = "b") #green
+ # points(land.kmeans$centers[4,], col=4, type = "b") #blue 西部平原農業區 1 2 22 32 38 40 60 61 63 
+ # points(land.kmeans$centers[5,], col=5, type = "b") #lblue 11 丘陵
+ # points(land.kmeans$centers[6,], col=6, type = "b") #purple 9 22 全台山地
+ # points(land.kmeans$centers[7,], col=7, type = "b") #gray 24 35 53 南部濱海
+ # test <- cbind(dataA, land.kmeans$cluster)
+ # test[land.kmeans$cluster==1, 2:3]
 
  # Dimension Reduction -------------------------------------------
  # Reduce a => 3 principal component (65%) & SIR
@@ -143,14 +143,13 @@ summary(c.sir)
 c.sir.comp <- as.matrix(c.comp$completeObs %*% as.matrix(c.sir$evectors[,1:3]))
 plot(c.sir.comp[,1:2], col=c.kmeans$cluster)
 
- # Reduce land => 2 principal component (63%) & ISOMAP
-  # S1:Overall trust in government agencies or institutions
-  # S2:Supporting to free speech and dissatisfaction of political officials
-
+ # Reduce land => 2 principal component (26%) & ISOMAP
+  # ISO1:Altitude
+  # ISO2:Housing to agriculture
 land.pca <- PCA(dataA[, 1:63], ncp=2, row.w = dataA$weight)
 get_eigenvalue(land.pca) #Variance
 par(mfrow=c(1, 2))
 plot(land.pca$ind$coord[, 1], land.pca$ind$coord[, 2], 
      xlab="PCA-1", ylab="PCA-2", col=land.kmeans$cluster)
-land.isomap <- isomap(dist(dataA[, 1:63]), ndim=2, k=100)
+land.isomap <- isomap(dist(dataA[, 1:63]), ndim=2, k=75)
 plot(land.isomap, col=land.kmeans$cluster)
